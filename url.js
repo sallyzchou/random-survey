c04d443dca462b1c8e42f8dd729d7ee806fbbe68
sallyzchou <120321152+sallyzchou@users.noreply.github.com>
2023-03-30 16:18:37 +0800

    update official survey url
    update official survey link

--- url.js
+++ url.js
@@ -8,13 +8,13 @@
 
 
 const urls = [
-	'https://www.surveycake.com/s/9qXAx',
-	'https://www.surveycake.com/s/xDZ2D',
-	'https://www.surveycake.com/s/8AvPQ',
-	'https://www.surveycake.com/s/7QNX3',
-	'https://www.surveycake.com/s/bpK8g',
-	'https://www.surveycake.com/s/34B7R',
-	'https://www.surveycake.com/s/QPzry',
-	'https://www.surveycake.com/s/k64k4',
-	'https://www.surveycake.com/s/Vg6Ln'
+	'https://www.surveycake.com/s/LPLDP',
+	'https://www.surveycake.com/s/Z6lo2',
+	'https://www.surveycake.com/s/MP3xV',
+	'https://www.surveycake.com/s/dpG4O',
+	'https://www.surveycake.com/s/p6YBz',
+	'https://www.surveycake.com/s/Y6MdZ',
+	'https://www.surveycake.com/s/vZ3ye',
+	'https://www.surveycake.com/s/KP4RY',
+	'https://www.surveycake.com/s/W6zPx'
 ];
