574ad6e479aee5b4fbe5bf8de3091efb787337fa
sallyzchou <120321152+sallyzchou@users.noreply.github.com>
2023-03-31 20:28:15 +0800

    Update formal survey links
    this version of survey links have modified the layout of message nudges(人身安全/風險）、大幅增加

--- url.js
+++ url.js
@@ -8,13 +8,13 @@
 
 
 const urls = [
-	'https://www.surveycake.com/s/LPLDP',
-	'https://www.surveycake.com/s/Z6lo2',
-	'https://www.surveycake.com/s/MP3xV',
-	'https://www.surveycake.com/s/dpG4O',
-	'https://www.surveycake.com/s/p6YBz',
-	'https://www.surveycake.com/s/Y6MdZ',
-	'https://www.surveycake.com/s/vZ3ye',
-	'https://www.surveycake.com/s/KP4RY',
-	'https://www.surveycake.com/s/W6zPx'
+	'https://www.surveycake.com/s/mZxQ6',
+	'https://www.surveycake.com/s/VgVky',
+	'https://www.surveycake.com/s/Z6le2',
+	'https://www.surveycake.com/s/lZWyL',
+	'https://www.surveycake.com/s/dpG7O',
+	'https://www.surveycake.com/s/p6YKz',
+	'https://www.surveycake.com/s/1Ol2K',
+	'https://www.surveycake.com/s/DPxZR',
+	'https://www.surveycake.com/s/g6v2Z'
 ];
